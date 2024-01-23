74d57a4f49b41ed1f7574013a46b33bd0c5b0fa0
paula258295 <156890213+paula258295@users.noreply.github.com>
2024-01-23 22:16:28 +0100

    Update test_my_module.py

--- test_my_module.py
+++ test_my_module.py
@@ -1,10 +1,10 @@
 import pytest
-from my_module import add
+from my_module import add_numbers
 import pytest
 
-def test_add():
-    assert add(8, 1) == 9
-    assert add(1, 1) == 2
-    assert add(-1, 1) == 0
-    assert add(-5, -2) == -7
+def test_add_numbers():
+    assert add_numbers(8, 1) == 9
+    assert add_numbers(1, 1) == 2
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(-5, -2) == -7
 
